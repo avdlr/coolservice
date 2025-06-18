40773375f832150e1286225773557a4ddb941627
avdlr <headkorn14@gmail.com>
2025-06-18 15:38:53 -0700

    Remove accidentally committed compiled classes

--- jsp/MaintenanceForm.jsp
+++ jsp/MaintenanceForm.jsp
@@ -1,7 +1,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@ page import="clases.Activity"%>
-<%@ page import="clases.Section"%>
+<%@ page import="zntclases.Activity"%>
+<%@ page import="zntclases.Section"%>
 <%@ page import="servlet.MaintenanceFormServlet"%>
 
 <html>
---
+++ src/zntclases/Activity.java
@@ -0,0 +1,29 @@
+package zntclases;
+
+/**
+ * Simple representation of a checklist activity.
+ */
+public class Activity {
+    private String name;
+
+    /**
+     * Create an Activity with the given name.
+     *
+     * @param name descriptive name of the activity
+     */
+    public Activity(String name) {
+        this.name = name;
+    }
+
+    /** Default constructor. */
+    public Activity() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
